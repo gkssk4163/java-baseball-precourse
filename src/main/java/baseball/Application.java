78a4b2f56a03661fb6a234f89e9de36f62499de6
hanna <gkssk4163@hanmail.net>
2021-10-06 15:16:42 +0900

    feat: main 구현 및 테스트 작성
    게임시작 및 게임종료 후 다시진행여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import domain.Match;
+import domain.Numbers;
+import domain.UserNumbers;
+import utils.UserConsole;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        do {
+            startGame();
+        } while (UserConsole.continueGame());
+    }
+
+    private static void startGame() {
+        List<Integer> numbers = Numbers.generate();
+
+        List<Integer> userNumbers;
+        do {
+            userNumbers = UserNumbers.parseUserNumbers(UserConsole.enterNumbers());
+        } while (!Match.correctAnswer(numbers, userNumbers));
     }
 }
