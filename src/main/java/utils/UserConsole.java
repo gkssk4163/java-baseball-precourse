9241de0603e52db95809af554400a9dc5fa70c52
hanna <gkssk4163@hanmail.net>
2021-10-06 10:39:10 +0900

    feat: 사용자 입력 기능 구현
    사용자 숫자입력 및 게임 지속여부 입력기능 구현

---
+++ src/main/java/utils/UserConsole.java
@@ -0,0 +1,15 @@
+package utils;
+
+import nextstep.utils.Console;
+
+public class UserConsole {
+	public static String enterNumbers() {
+		System.out.println("숫자를 입력해주세요 : ");
+		return Console.readLine();
+	}
+
+	public static boolean continueGame() {
+		System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ");
+		return Console.readLine().equals("1");
+	}
+}
